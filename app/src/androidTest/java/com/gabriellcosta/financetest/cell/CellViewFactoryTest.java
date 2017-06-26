f929ca61d049c6b5b46960a56350ef106b6a348c
Gabriel Costa <gabrielcosta08@gmail.com>
2017-06-26 20:55:38 -0300

    Update CellViewFactory
    Make return instance for every Cell type

--- app/src/androidTest/java/com/gabriellcosta/financetest/cell/CellViewFactoryTest.java
+++ app/src/androidTest/java/com/gabriellcosta/financetest/cell/CellViewFactoryTest.java
@@ -1,8 +1,17 @@
 package com.gabriellcosta.financetest.cell;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.test.InstrumentationRegistry;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.gabriellcosta.financetest.entity.CellVO;
+import com.gabriellcosta.financetest.util.ReaderUtil;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,8 +23,39 @@ public class CellViewFactoryTest {
     factory = new CellViewFactory(InstrumentationRegistry.getTargetContext());
   }
 
-  @Test public void whenCreate_ShouldNotReturnNull() {
+  @Test(expected = IllegalArgumentException.class)
+  public void whenCreate_ShouldNotReturnNull() {
     assertNotNull(factory.create(null));
+  }
+
+  @Test public void whenCreateWithType1_ShouldCreateTextInputLayout() {
+    CellVO read = ReaderUtil.read("cell_field.json", CellVO.class);
+    View view = factory.create(read);
+    assertTrue(view instanceof TextInputLayout);
+  }
+
+  @Test public void whenCreateWithType2_ShouldCreateTextView() {
+    CellVO read = ReaderUtil.read("cell_text.json", CellVO.class);
+    View view = factory.create(read);
+    assertTrue(view instanceof TextView);
+  }
+
+  @Test public void whenCreateWithType3_ShouldCreateImage() {
+    CellVO read = ReaderUtil.read("cell_image.json", CellVO.class);
+    View view = factory.create(read);
+    assertTrue(view instanceof ImageView);
+  }
+
+  @Test public void whenCreateWithType4_ShouldCreateCheckbox() {
+    CellVO read = ReaderUtil.read("cell_checkbox.json", CellVO.class);
+    View view = factory.create(read);
+    assertTrue(view instanceof CheckBox);
+  }
+
+  @Test public void whenCreateWithType5_ShouldCreateButton() {
+    CellVO read = ReaderUtil.read("cell_send.json", CellVO.class);
+    View view = factory.create(read);
+    assertTrue(view instanceof Button);
   }
 
 }
--- app/src/main/java/com/gabriellcosta/financetest/cell/CellViewFactory.java
+++ app/src/main/java/com/gabriellcosta/financetest/cell/CellViewFactory.java
@@ -3,7 +3,13 @@ package com.gabriellcosta.financetest.cell;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.gabriellcosta.financetest.R;
 import com.gabriellcosta.financetest.entity.CellVO;
 import java.lang.ref.WeakReference;
 
@@ -15,8 +21,30 @@ public final class CellViewFactory {
     this.context = new WeakReference<>(context);
   }
 
-  @NonNull public View create(final CellVO cellVO) {
-    return new View(context.get());
+  @NonNull
+  public View create(final CellVO cellVO) {
+
+    if (cellVO == null) {
+      throw new IllegalArgumentException(
+          String.format("%s should not be null", CellVO.class.getSimpleName()));
+    }
+
+    switch (cellVO.getType()) {
+      case CellVO.TYPE_FIELD:
+        context.get().setTheme(R.style.AppTheme);
+        return new TextInputLayout(context.get());
+      case CellVO.TYPE_TEXT:
+        return new TextView(context.get());
+      case CellVO.TYPE_IMAGE:
+        return new ImageView(context.get());
+      case CellVO.TYPE_CHECKBOX:
+        return new CheckBox(context.get());
+      case CellVO.TYPE_SEND:
+        return new Button(context.get());
+      default:
+        return new View(context.get());
+    }
+
   }
 
 }
