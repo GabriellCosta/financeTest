446cf9ca48940b8776dc725d9e1158a601db1a41
Gabriel Costa <gabrielcosta08@gmail.com>
2017-06-26 23:22:21 -0300

    Add Top padding to views created by parser

--- app/src/main/java/com/gabriellcosta/financetest/cell/CellViewParser.java
+++ app/src/main/java/com/gabriellcosta/financetest/cell/CellViewParser.java
@@ -1,8 +1,10 @@
 package com.gabriellcosta.financetest.cell;
 
 
+import android.content.Context;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,18 +23,21 @@ final class CellViewParser {
     view.addView(child);
     child.setHint(cellVO.getMessage());
     view.setVisibility(cellVO.isHidden() ? View.GONE : View.VISIBLE);
+    view.setPadding(0, convertToDP(view.getContext(), cellVO.getTopSpacing()), 0, 0);
     return view;
   }
 
   static TextView parse(final TextView view, final CellVO cellVO) {
     view.setText(cellVO.getMessage());
     view.setVisibility(cellVO.isHidden() ? View.GONE : View.VISIBLE);
+    view.setPadding(0, convertToDP(view.getContext(), cellVO.getTopSpacing()), 0, 0);
     return view;
   }
 
   static ImageView parse(final ImageView view, final CellVO cellVO) {
     view.setContentDescription(cellVO.getMessage());
     view.setVisibility(cellVO.isHidden() ? View.GONE : View.VISIBLE);
+    view.setPadding(0, convertToDP(view.getContext(), cellVO.getTopSpacing()), 0, 0);
 
     Picasso.with(view.getContext())
         .load(cellVO.getMessage());
@@ -43,13 +48,20 @@ final class CellViewParser {
   static CheckBox parse(final CheckBox view, final CellVO cellVO) {
     view.setText(cellVO.getMessage());
     view.setVisibility(cellVO.isHidden() ? View.GONE : View.VISIBLE);
+    view.setPadding(0, convertToDP(view.getContext(), cellVO.getTopSpacing()), 0, 0);
     return view;
   }
 
   static Button parse(final Button view, final CellVO cellVO) {
     view.setText(cellVO.getMessage());
     view.setVisibility(cellVO.isHidden() ? View.GONE : View.VISIBLE);
+    view.setPadding(0, convertToDP(view.getContext(), cellVO.getTopSpacing()), 0, 0);
     return view;
+  }
+
+  private static int convertToDP(final Context context, final double value) {
+    return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (float) value,
+        context.getResources().getDisplayMetrics());
   }
 
 }
