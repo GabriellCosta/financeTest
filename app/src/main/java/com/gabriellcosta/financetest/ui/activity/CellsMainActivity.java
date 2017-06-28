5b58ef4a1da31858545411e402252dd14f9f9038
Gabriel Costa <gabrielcosta08@gmail.com>
2017-06-27 21:46:25 -0300

    Create Activity for Cells Service

---
+++ app/src/main/java/com/gabriellcosta/financetest/ui/activity/CellsMainActivity.java
@@ -0,0 +1,14 @@
+package com.gabriellcosta.financetest.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.gabriellcosta.financetest.R;
+
+public class CellsMainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_cells_main);
+  }
+}
